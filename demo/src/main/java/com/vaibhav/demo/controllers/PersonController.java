4420bfec03dd5c1cc05d4c803b7923c5a135a213
admin <admin@Toshiba-C55>
2017-11-08 17:59:34 +0530

    1) Added springMVC jars(4.3.7) cause old (4.3.11) was causing conversion problem 2) Added jackson dependency for @responsebody conversion 3) Added new method in person controller that only shows json as response http://localhost:8080/demo/person/personlistjson which show all persons list.

--- demo/src/main/java/com/vaibhav/demo/controllers/PersonController.java
+++ demo/src/main/java/com/vaibhav/demo/controllers/PersonController.java
@@ -9,12 +9,15 @@ import org.apache.commons.logging.Log;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -90,6 +93,15 @@ public class PersonController extends CommonController {
 		}
 		
 		return mv;
+	}
+	 
+	@ResponseBody
+	@RequestMapping(value = "/personlistjson")
+	public ArrayList<PersonTO> getPersonJsonList()
+	{
+		ArrayList<PersonTO> personlist = (ArrayList<PersonTO>) persondao.searchPerson(null);
+		logger.debug("Json list:" + personlist);
+		return personlist;
 	}
 	
 	//RedirectAttributes redirectAttributes
